0b7e9fccc48044a335c50465ae1e319196bce97e
Hernán Alegre <hernan.cs@hotmail.com>
2025-08-26 10:06:19 -0300

    Revert "prueba subiendo .env y  db.js"
    This reverts commit 5528cb3e451277775e65cfced2868c139453003c.

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,16 +3,6 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-});
-
-module.exports = pool;
-
-/* const pool = new Pool({
   host: process.env.DB_HOST,     // ej: 'localhost'
   port: process.env.DB_PORT,     // ej: 5432
   user: process.env.DB_USER,     // ej: 'postgres'
@@ -20,4 +10,4 @@ module.exports = pool;
   database: process.env.DB_NAME, // el nombre de tu base de datos
 });
 
-module.exports = pool; */
+module.exports = pool;

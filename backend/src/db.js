d09d717db64fc34fad2b75868bd264e32e0d1e97
Hernán Alegre <hernan.cs@hotmail.com>
2025-08-26 13:18:47 -0300

    Revert "Ignorar .env y db.js correctamente"
    This reverts commit b8b8ad5c4a39ca6191435e8d0e7d5ae86cf6b873.

---
+++ backend/src/db.js
@@ -0,0 +1,21 @@
+// src/db.js
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  /* host: process.env.PGHOST, // ej: 'localhost'
+  port: process.env.PGPORT, // ej: 5432
+  user: process.env.PGUSER, // ej: 'postgres'
+  password: process.env.PGPASSWORD, // tu contraseña de PostgreSQL
+  database: process.env.PGDATABASE, // el nombre de tu base de datos 
+  PGD_SECRET: process.env.PGD_SECRET,*/
+
+  host: process.env.DB_HOST, // ej: 'localhost'
+  port: process.env.DB_PORT, // ej: 5432
+  user: process.env.DB_USER, // ej: 'postgres'
+  password: process.env.DB_PASS, // tu contraseña de PostgreSQL
+  database: process.env.DB_NAME, // el nombre de tu base de datos
+  JWT_SECRET: process.env.JWT_SECRET,
+});
+
+module.exports = pool;

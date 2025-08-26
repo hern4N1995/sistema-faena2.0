eb775bd5f7b79599e976c3990e20ebe6377ed063
enece-i25 <104716531+enece-i25@users.noreply.github.com>
2025-08-26 13:09:15 -0300

    Correccion .env

--- backend/src/db.js
+++ backend/src/db.js
@@ -7,12 +7,15 @@ const pool = new Pool({
   port: process.env.PGPORT, // ej: 5432
   user: process.env.PGUSER, // ej: 'postgres'
   password: process.env.PGPASSWORD, // tu contraseña de PostgreSQL
-  database: process.env.PGDATABASE, // el nombre de tu base de datos */
+  database: process.env.PGDATABASE, // el nombre de tu base de datos 
+  PGD_SECRET: process.env.PGD_SECRET,*/
+
   host: process.env.DB_HOST, // ej: 'localhost'
   port: process.env.DB_PORT, // ej: 5432
   user: process.env.DB_USER, // ej: 'postgres'
   password: process.env.DB_PASS, // tu contraseña de PostgreSQL
   database: process.env.DB_NAME, // el nombre de tu base de datos
+  JWT_SECRET: process.env.JWT_SECRET,
 });
 
 module.exports = pool;

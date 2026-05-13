000d4ee7bae61b45142b207d93b14e6487166edd
enece-i25 <104716531+enece-i25@users.noreply.github.com>
2026-05-13 08:39:22 -0300

    Update app.js

--- backend/src/App.js
+++ backend/src/App.js
@@ -279,7 +279,7 @@ app.use((req, res, next) => {
       );
       res.setHeader(
         'Access-Control-Allow-Headers',
-        'Content-Type, Authorization, X-Requested-With',
+        'Content-Type, Authorization, X-Requested-With, X-CSRF-Token',
       );
       if (req.method === 'OPTIONS') return res.sendStatus(204);
       return next();
